29de5ddf62eb9134d525f94e5fa50c3c53a7de95
mohamed-ndoye611 <mohamed@diamondy.dev>
2025-03-12 01:15:40 +0100

    style(sidebar): Adjust sizes

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -46,7 +46,7 @@ const getIcon = (id: string) => defineAsyncComponent(() => import(`@/public/imag
         <nav>
             <ul class="flex flex-col gap-2">
                 <NuxtLink
-                    class="navlink flex items-center gap-2 px-6 py-5 text-[18px] w-full rounded-xl hover:bg-accent/20 transition duration-300 ease-in-out"
+                    class="navlink flex items-center gap-2 px-6 py-4 text-[18px] w-full rounded-xl hover:bg-accent/20 transition duration-300 ease-in-out"
                     v-for="(navLink, index) in NAVLINKS" :key="index" :to="navLink.url">
                     <component :is="getIcon(navLink.icon)"
                         class="stroke-secondary navlink-[.active]:stroke-primary fill-none aspect-square h-6 stroke-2" />
@@ -55,7 +55,7 @@ const getIcon = (id: string) => defineAsyncComponent(() => import(`@/public/imag
             </ul>
         </nav>
         <button
-            class="flex cursor-pointer transition duration-300 ease-in-out hover:bg-secondary/85 items-center gap-2 bg-secondary mt-auto text-primary text-left px-6 py-5 text-[18px] leading-4 w-full rounded-xl">
+            class="flex cursor-pointer transition duration-300 ease-in-out hover:bg-secondary/85 items-center gap-2 bg-secondary mt-auto text-primary text-left px-6 py-4 text-[18px] leading-4 w-full rounded-xl">
             <component :is="getIcon('log-out')"
                 class="stroke-primary navlink-[.active]:stroke-primary fill-none aspect-square h-6 stroke-2" />
             <span class="relative top-0.5">Log out</span>
