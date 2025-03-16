a76628b9a6526667561e550eaaafa042b6563875
mohamed-ndoye611 <mohamed@diamondy.dev>
2025-03-16 09:44:04 +0100

    hotfix: Add missing files

---
+++ layouts/dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <AppSidebar />
+    <main class="w-full px-10 py-6 z-10 relative h-screen overflow-y-auto flex flex-col">
+        <slot />
+    </main>
+    <div
+        class="sphere bg-accent/10 h-[100vw] w-[500vw] block fixed top-[60vh] left-[30vw] rounded-full blur-[128px] z-[-1]">
+    </div>
+</template>
